9ce6c13e24848acfdb570ecb88ce468e2c1a6cbd
Alex <agoldstone@umass.edu>
2022-11-21 23:51:44 -0500

    AG 2

--- fetchUniversities.js
+++ fetchUniversities.js
@@ -1,3 +1,10 @@
 export function fetchUniversities(query) {
   // TODO
+  const searchURL = new URL('https://university-web-api.herokuapp.com/search');
+  searchURL.searchParams.append("name", query);
+  return fetch(searchURL.toString())
+  .then(response => response.json())
+  .then(data => data.map(uni => uni.name))
+  .catch(new Error("No results found"));
 }
+
